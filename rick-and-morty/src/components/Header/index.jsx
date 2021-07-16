8f5832e1ada84d9faf205104b91a1eb5520a4a88
burcukaragozzz <burcukaragoz1515@gmail.com>
2021-07-16 16:47:04 +0300

    add Header component

---
+++ rick-and-morty/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+  return(
+    <header className="img-container">
+      <img 
+        src="https://occ-0-1068-1723.1.nflxso.net/dnm/api/v6/TsSRXvDuraoJ7apdkH6tsHhf-ZQ/AAAABbtnw6C35mhluezr-K_FiP65TW93xpH0M3S6lKPv50_7eshzc1vosWxE3CxcnJ5-eVYmpcaQyra9yFLzQtsNx4odwYO-GtDDmDU0.png?r=47e" 
+        height="180" 
+        alt="header-logo" 
+      />
+    </header>
+  )
+}
